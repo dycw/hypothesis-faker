0eb683244c580f8ab0cdb7ce5f81bf10227769bf
Derek Wan <d.wan@icloud.com>
2021-04-27 23:08:23 +0800

    Convert WeightedList to be a subclass of list (#3)
    * Write as floats
    
    * Convert WeightedList to be a subclass of list

--- src/tests/test_utilities.py
+++ src/tests/test_utilities.py
@@ -8,7 +8,7 @@ from hypothesis_faker.utilities import weighted_samples
 
 @given(i=floats(-1.0, 7.0, allow_nan=False, allow_infinity=False))
 def test_weighted_list(i: float) -> None:
-    wlist = WeightedList(("a", 3.0), ("b", 1.0), ("c", 2.0))
+    wlist = WeightedList([("a", 3.0), ("b", 1.0), ("c", 2.0)])
     assert len(wlist) == 3
     if 0.0 <= i < 3.0:
         assert wlist[i] == "a"
@@ -28,9 +28,9 @@ def test_weighted_list_cannot_be_empty() -> None:
 
 def test_weighted_list_cannot_have_negative_weights() -> None:
     with raises(ValueError):
-        WeightedList(("a", -1.0))
+        WeightedList([("a", -1.0)])
 
 
-@given(letter=weighted_samples(("a", 3), ("b", 1), ("c", 2)))
+@given(letter=weighted_samples([("a", 3.0), ("b", 1.0), ("c", 2.0)]))
 def test_weighted_samples(letter: str) -> None:
     assert letter in {"a", "b", "c"}
