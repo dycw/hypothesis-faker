fd972a639715a690ceb97931d6f3d9bbba295674
Derek Wan <d.wan@icloud.com>
2021-04-27 22:37:40 +0800

    Add two tests (#2)

--- src/tests/test_utilities.py
+++ src/tests/test_utilities.py
@@ -8,9 +8,9 @@ from hypothesis_faker.utilities import weighted_samples
 
 @given(i=floats(-1.0, 7.0, allow_nan=False, allow_infinity=False))
 def test_weighted_list(i: float) -> None:
-    wlist = WeightedList(("a", 3), ("b", 1), ("c", 2))
+    wlist = WeightedList(("a", 3.0), ("b", 1.0), ("c", 2.0))
     assert len(wlist) == 3
-    if 0 <= i < 3.0:
+    if 0.0 <= i < 3.0:
         assert wlist[i] == "a"
     elif 3.0 <= i < 4.0:
         assert wlist[i] == "b"
@@ -19,6 +19,16 @@ def test_weighted_list(i: float) -> None:
     else:
         with raises(IndexError):
             wlist[i]
+
+
+def test_weighted_list_cannot_be_empty() -> None:
+    with raises(ValueError):
+        WeightedList()
+
+
+def test_weighted_list_cannot_have_negative_weights() -> None:
+    with raises(ValueError):
+        WeightedList(("a", -1.0))
 
 
 @given(letter=weighted_samples(("a", 3), ("b", 1), ("c", 2)))
