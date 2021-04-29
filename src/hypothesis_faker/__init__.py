b98d93421f576426e69accfcfcbf04556e37c821
Derek Wan <d.wan@icloud.com>
2021-04-29 22:59:14 +0800

    Move some imports to the top

---
+++ src/hypothesis_faker/__init__.py
@@ -0,0 +1,8 @@
+from hypothesis_faker.providers.person import first_names_female  # noqa: F401
+from hypothesis_faker.providers.person import first_names_male  # noqa: F401
+from hypothesis_faker.providers.person import last_names  # noqa: F401
+from hypothesis_faker.providers.person import names  # noqa: F401
+from hypothesis_faker.providers.person import prefixes_female  # noqa: F401
+from hypothesis_faker.providers.person import prefixes_male  # noqa: F401
+from hypothesis_faker.providers.person import suffixes_female  # noqa: F401
+from hypothesis_faker.providers.person import suffixes_male  # noqa: F401
