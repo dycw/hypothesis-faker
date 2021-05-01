30f89cc00fae895711e41f8aa7c976ee4b098caa
Derek Wan <d.wan@icloud.com>
2021-05-01 21:38:49 +0800

    Implement the rest of the strategies for person (#11)
    * Implement the rest of the strategies for person
    
    * Add rest of last_names/prefixes/suffixes/language_names

--- src/hypothesis_faker/__init__.py
+++ src/hypothesis_faker/__init__.py
@@ -3,11 +3,27 @@ from hypothesis_faker.providers.lorem import paragraphs  # noqa: F401
 from hypothesis_faker.providers.lorem import sentences  # noqa: F401
 from hypothesis_faker.providers.lorem import texts  # noqa: F401
 from hypothesis_faker.providers.lorem import words  # noqa: F401
+from hypothesis_faker.providers.person import first_names  # noqa: F401
 from hypothesis_faker.providers.person import first_names_female  # noqa: F401
 from hypothesis_faker.providers.person import first_names_male  # noqa: F401
+from hypothesis_faker.providers.person import first_names_nonbinary
+from hypothesis_faker.providers.person import language_names  # noqa: F401
 from hypothesis_faker.providers.person import last_names  # noqa: F401
+from hypothesis_faker.providers.person import last_names_female  # noqa: F401
+from hypothesis_faker.providers.person import last_names_male  # noqa: F401
+from hypothesis_faker.providers.person import last_names_nonbinary  # noqa: F401
 from hypothesis_faker.providers.person import names  # noqa: F401
+from hypothesis_faker.providers.person import names_female  # noqa: F401
+from hypothesis_faker.providers.person import names_male  # noqa: F401
+from hypothesis_faker.providers.person import names_nonbinary  # noqa: F401
+from hypothesis_faker.providers.person import prefixes  # noqa: F401
 from hypothesis_faker.providers.person import prefixes_female  # noqa: F401
 from hypothesis_faker.providers.person import prefixes_male  # noqa: F401
+from hypothesis_faker.providers.person import prefixes_nonbinary  # noqa: F401
+from hypothesis_faker.providers.person import suffixes  # noqa: F401
 from hypothesis_faker.providers.person import suffixes_female  # noqa: F401
 from hypothesis_faker.providers.person import suffixes_male  # noqa: F401
+from hypothesis_faker.providers.person import suffixes_nonbinary  # noqa: F401
+
+
+_ = first_names_nonbinary
--- src/hypothesis_faker/providers/person.py
+++ src/hypothesis_faker/providers/person.py
@@ -1,52 +1,55 @@
-import re
 from typing import Callable
 
+from faker.providers.person import Provider as PersonProvider
 from faker.providers.person.en_US import Provider as PersonProviderEnUS
 from hypothesis.strategies import SearchStrategy
 from hypothesis.strategies import composite
 from hypothesis.strategies import fixed_dictionaries
+from hypothesis.strategies import sampled_from
 
 from hypothesis_faker.types import T
+from hypothesis_faker.utilities import PATTERN_FOR_DOUBLE_BRACES
 from hypothesis_faker.utilities import WeightedList
 from hypothesis_faker.utilities import fill_format_string
 from hypothesis_faker.utilities import weighted_samples
 
 
+_LANGUAGE_NAMES = PersonProvider.language_names
+_FORMATS_MALE: WeightedList[str] = WeightedList(
+    PersonProviderEnUS.formats_male.items()
+)
 _FORMATS_FEMALE: WeightedList[str] = WeightedList(
     PersonProviderEnUS.formats_female.items()
 )
 _FORMATS_NON_BINARY: WeightedList[str] = WeightedList(
     PersonProviderEnUS.formats_nonbinary.items()
 )
-_FORMATS_MALE: WeightedList[str] = WeightedList(
-    PersonProviderEnUS.formats_male.items()
-)
 _FORMATS = _FORMATS_MALE + _FORMATS_FEMALE
-_FIRST_NAMES_FEMALE: WeightedList[str] = WeightedList(
-    PersonProviderEnUS.first_names_female.items()
-)
 _FIRST_NAMES_MALE: WeightedList[str] = WeightedList(
     PersonProviderEnUS.first_names_male.items()
 )
-_FIRST_NAMES = _FIRST_NAMES_FEMALE + _FIRST_NAMES_MALE
+_FIRST_NAMES_FEMALE: WeightedList[str] = WeightedList(
+    PersonProviderEnUS.first_names_female.items()
+)
+_FIRST_NAMES = _FORMATS_MALE + _FIRST_NAMES_FEMALE
 _FIRST_NAMES_NONBINARY = _FIRST_NAMES_MALE + _FIRST_NAMES_FEMALE
 _LAST_NAMES: WeightedList[str] = WeightedList(
     PersonProviderEnUS.last_names.items()
 )
-_PREFIXES_FEMALE: WeightedList[str] = WeightedList(
-    PersonProviderEnUS.prefixes_female.items()
-)
 _PREFIXES_MALE: WeightedList[str] = WeightedList(
     PersonProviderEnUS.prefixes_male.items()
+)
+_PREFIXES_FEMALE: WeightedList[str] = WeightedList(
+    PersonProviderEnUS.prefixes_female.items()
 )
 _PREFIXES_NONBINARY: WeightedList[str] = WeightedList(
     PersonProviderEnUS.prefixes_nonbinary.items()
 )
-_SUFFIXES_FEMALE: WeightedList[str] = WeightedList(
-    PersonProviderEnUS.suffixes_female.items()
-)
 _SUFFIXES_MALE: WeightedList[str] = WeightedList(
     PersonProviderEnUS.suffixes_male.items()
+)
+_SUFFIXES_FEMALE: WeightedList[str] = WeightedList(
+    PersonProviderEnUS.suffixes_female.items()
 )
 _SUFFIXES_NONBINARY: WeightedList[str] = WeightedList(
     PersonProviderEnUS.suffixes_nonbinary.items()
@@ -55,35 +58,55 @@ _SUFFIXES_NONBINARY: WeightedList[str] = WeightedList(
 # simple
 
 
-first_names_female = weighted_samples(_FIRST_NAMES_FEMALE)
 first_names_male = weighted_samples(_FIRST_NAMES_MALE)
+first_names_female = weighted_samples(_FIRST_NAMES_FEMALE)
+first_names_nonbinary = weighted_samples(_FIRST_NAMES_NONBINARY)
+first_names = weighted_samples(_FIRST_NAMES)
 last_names = weighted_samples(_LAST_NAMES)
-prefixes_female = weighted_samples(_PREFIXES_FEMALE)
+last_names_male = last_names
+last_names_female = last_names
+last_names_nonbinary = last_names
 prefixes_male = weighted_samples(_PREFIXES_MALE)
-suffixes_female = weighted_samples(_SUFFIXES_FEMALE)
+prefixes_female = weighted_samples(_PREFIXES_FEMALE)
+prefixes_nonbinary = weighted_samples(_PREFIXES_NONBINARY)
+prefixes = prefixes_male | prefixes_female | prefixes_nonbinary
 suffixes_male = weighted_samples(_SUFFIXES_MALE)
+suffixes_female = weighted_samples(_SUFFIXES_FEMALE)
+suffixes_nonbinary = weighted_samples(_SUFFIXES_NONBINARY)
+suffixes = suffixes_male | suffixes_female | suffixes_nonbinary
+language_names = sampled_from(_LANGUAGE_NAMES)
 
 
-# compound
+# name
 
 
-@composite
-def _names(draw: Callable[[SearchStrategy[T]], T]) -> str:
-    format_ = draw(weighted_samples(_FORMATS))
-    tokens = _PATTERN_FOR_DOUBLE_BRACES.findall(format_)
-    strategies = {token: _TOKENS_TO_STRATEGIES[token] for token in tokens}
-    replacements = draw(fixed_dictionaries(strategies))
-    return fill_format_string(format_, replacements)
+def _build_names_strategy(formats: WeightedList[str]) -> SearchStrategy[str]:
+    @composite
+    def inner(draw: Callable[[SearchStrategy[T]], T]) -> str:
+        format_ = draw(weighted_samples(formats))
+        tokens = PATTERN_FOR_DOUBLE_BRACES.findall(format_)
+        strategies = {token: _TOKENS_TO_STRATEGIES[token] for token in tokens}
+        replacements = draw(fixed_dictionaries(strategies))
+        return fill_format_string(format_, replacements)
+
+    return inner()
 
 
-names = _names()
-_PATTERN_FOR_DOUBLE_BRACES = re.compile(r"{{(\w+)}}")
 _TOKENS_TO_STRATEGIES = {
     "first_name_female": first_names_female,
     "first_name_male": first_names_male,
+    "first_name_nonbinary": first_names_nonbinary,
     "last_name": last_names,
     "prefix_female": prefixes_female,
     "prefix_male": prefixes_male,
+    "prefix_nonbinary": prefixes_nonbinary,
     "suffix_female": suffixes_female,
     "suffix_male": suffixes_male,
+    "suffix_nonbinary": suffixes_nonbinary,
 }
+
+
+names_male = _build_names_strategy(_FORMATS_MALE)
+names_female = _build_names_strategy(_FORMATS_FEMALE)
+names_nonbinary = _build_names_strategy(_FORMATS_NON_BINARY)
+names = _build_names_strategy(_FORMATS)
