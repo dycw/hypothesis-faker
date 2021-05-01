6dbc3d9bfdaa225903466657075ee402fed41bef
Derek Wan <d.wan@icloud.com>
2021-05-01 20:56:20 +0800

    Add paragraphs, texts (#10)

--- src/hypothesis_faker/providers/lorem.py
+++ src/hypothesis_faker/providers/lorem.py
@@ -1,7 +1,9 @@
+from itertools import chain
 from typing import Callable
 
 from faker.providers.lorem import Provider as LoremProvider
 from faker.providers.lorem.en_US import Provider as LoremProviderEnUS
+from hypothesis.errors import InvalidArgument
 from hypothesis.strategies import SearchStrategy
 from hypothesis.strategies import composite
 from hypothesis.strategies import lists
@@ -20,8 +22,54 @@ words = sampled_from(_WORD_LIST)
 
 @composite
 def _sentences(draw: Callable[[SearchStrategy[T]], T]) -> str:
-    drawn_words = draw(lists(words))
-    return _WORD_CONNECTOR.join(drawn_words) + _SENTENCE_PUNCTUATION
+    if drawn := draw(lists(words)):
+        return _join_words_into_sentence(drawn)
+    else:
+        return ""
 
 
 sentences = _sentences()
+
+
+@composite
+def _paragraphs(draw: Callable[[SearchStrategy[T]], T]) -> str:
+    if drawn := draw(lists(sentences)):
+        return _WORD_CONNECTOR.join(drawn)
+    else:
+        return ""
+
+
+paragraphs = _paragraphs()
+
+
+def texts(max_length: int = 200) -> SearchStrategy[str]:
+    @composite
+    def inner(draw: Callable[[SearchStrategy[T]], T]) -> str:
+        parts = []
+        if max_length < 5:
+            raise InvalidArgument(f"{max_length=} must be at least 5")
+        elif 5 <= max_length < 25:
+            while sum(map(len, parts)) < max_length:
+                parts.append(draw(words))
+            return _join_words_into_sentence(parts)
+        else:
+            if 25 <= max_length < 100:
+                raw_strategy = sentences
+                sep = _WORD_CONNECTOR
+            else:
+                raw_strategy = paragraphs
+                sep = "\n"
+            strategy = raw_strategy.filter(bool)
+            while sum(map(len, parts)) < max_length:
+                parts.append(draw(strategy))
+            return sep.join(parts)
+
+    return inner()
+
+
+def _join_words_into_sentence(words: list[str]) -> str:
+    first, *rest = words
+    return (
+        _WORD_CONNECTOR.join(chain([first.title()], rest))
+        + _SENTENCE_PUNCTUATION
+    )
--- src/tests/providers/test_lorem.py
+++ src/tests/providers/test_lorem.py
@@ -1,14 +1,27 @@
+from hypothesis import HealthCheck
 from hypothesis import given
+from hypothesis import settings
 from hypothesis.strategies import DataObject
 from hypothesis.strategies import SearchStrategy
 from hypothesis.strategies import data
+from hypothesis.strategies import integers
 from pytest import mark
 
+from hypothesis_faker.providers.lorem import paragraphs
 from hypothesis_faker.providers.lorem import sentences
+from hypothesis_faker.providers.lorem import texts
 from hypothesis_faker.providers.lorem import words
 
 
-@mark.parametrize("strategy", [words, sentences])
+@mark.parametrize("strategy", [words, sentences, paragraphs])
 @given(data=data())
-def test_words(data: DataObject, strategy: SearchStrategy[str]) -> None:
+def test_words_sentences_and_paragraphs(
+    data: DataObject, strategy: SearchStrategy[str]
+) -> None:
     assert isinstance(data.draw(strategy), str)
+
+
+@given(max_length=integers(5, 300), data=data())
+@settings(suppress_health_check=[HealthCheck.too_slow])
+def test_texts(max_length: int, data: DataObject) -> None:
+    assert isinstance(data.draw(texts(max_length)), str)
