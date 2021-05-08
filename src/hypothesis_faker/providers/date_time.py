17a460d676be169185a2d71bd8a2eb02655b4ba1
Derek Wan <d.wan@icloud.com>
2021-05-08 18:51:46 +0800

    More tests

--- src/hypothesis_faker/providers/date_time.py
+++ src/hypothesis_faker/providers/date_time.py
@@ -3,10 +3,23 @@ from __future__ import annotations
 import datetime as dt
 from datetime import tzinfo
 
+from faker.providers.date_time import datetime_to_timestamp
 from hypothesis.strategies import SearchStrategy
 from hypothesis.strategies import datetimes
 from hypothesis.strategies import just
 from hypothesis.strategies import sampled_from
+
+
+def unix_times(
+    end_datetime: dt.datetime | None = None,
+    start_datetime: dt.datetime | None = None,
+) -> SearchStrategy[int]:
+    return datetimes(
+        min_value=dt.datetime(1970, 1, 1)
+        if start_datetime is None
+        else start_datetime,
+        max_value=dt.datetime.now() if end_datetime is None else end_datetime,
+    ).map(datetime_to_timestamp)
 
 
 def date_times(
@@ -25,6 +38,42 @@ def date_times_ad(
     return _date_times_between(
         tzinfo=tzinfo, end_datetime=end_datetime, min_value=dt.datetime(1, 1, 1)
     )
+
+
+def iso8601s(
+    tzinfo: tzinfo | None = None, end_datetime: dt.datetime | None = None
+) -> SearchStrategy[str]:
+    return date_times(tzinfo=tzinfo, end_datetime=end_datetime).map(
+        lambda dt: dt.isoformat()
+    )
+
+
+def dates(
+    pattern: str = "%Y-%m-%d", end_datetime: dt.datetime | None = None
+) -> SearchStrategy[str]:
+    return date_times(end_datetime=end_datetime).map(
+        lambda dt: dt.strftime(pattern)
+    )
+
+
+def date_objects(
+    end_datetime: dt.datetime | None = None,
+) -> SearchStrategy[dt.date]:
+    return date_times(end_datetime=end_datetime).map(lambda dt: dt.date())
+
+
+def times(
+    pattern: str = "%H:%M:%S", end_datetime: dt.datetime | None = None
+) -> SearchStrategy[str]:
+    return date_times(end_datetime=end_datetime).map(
+        lambda dt: dt.time().strftime(pattern)
+    )
+
+
+def time_objects(
+    end_datetime: dt.datetime | None = None,
+) -> SearchStrategy[dt.time]:
+    return date_times(end_datetime=end_datetime).map(lambda dt: dt.time())
 
 
 am_pms = sampled_from(["AM", "PM"])
--- src/tests/providers/test_date_time.py
+++ src/tests/providers/test_date_time.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 import datetime as dt
 from datetime import tzinfo
 from typing import Callable
-from typing import Optional
 
+from hypothesis import assume
 from hypothesis import given
+from hypothesis.errors import InvalidArgument
 from hypothesis.strategies import DataObject
 from hypothesis.strategies import SearchStrategy
 from hypothesis.strategies import data
@@ -16,11 +19,49 @@ from pytz import UTC
 from hypothesis_faker.providers.date_time import am_pms
 from hypothesis_faker.providers.date_time import date_times
 from hypothesis_faker.providers.date_time import date_times_ad
+from hypothesis_faker.providers.date_time import unix_times
 
 
 @given(am_pm=am_pms)
 def test_am_pms(am_pm: str) -> None:
     assert am_pm in {"AM", "PM"}
+
+
+@given(
+    data=data(),
+    start_datetime=datetimes() | none(),
+    end_datetime=datetimes() | none(),
+)
+def test_unix_times(
+    data: DataObject,
+    start_datetime: dt.datetime | None,
+    end_datetime: dt.datetime | None,
+) -> None:
+    try:
+        unix = data.draw(
+            unix_times(start_datetime=start_datetime, end_datetime=end_datetime)
+        )
+    except InvalidArgument:
+        assume(False)
+    else:
+        assert isinstance(unix, int)
+        datetime = dt.datetime.fromtimestamp(unix, tz=UTC).replace(tzinfo=None)
+        buffer = dt.timedelta(seconds=1)  # 1 second buffer needed
+        assert (
+            (
+                (
+                    dt.datetime(1970, 1, 1)
+                    if start_datetime is None
+                    else start_datetime
+                )
+                - buffer
+            )
+            <= datetime
+            <= (
+                (dt.datetime.now() if end_datetime is None else end_datetime)
+                + buffer
+            )
+        )
 
 
 @mark.parametrize(
@@ -33,7 +74,7 @@ def test_am_pms(am_pm: str) -> None:
 @given(data=data())
 def test_date_times(
     strategy: Callable[
-        [Optional[tzinfo], Optional[dt.datetime]], SearchStrategy[dt.datetime]
+        [tzinfo | None, dt.datetime | None], SearchStrategy[dt.datetime]
     ],
     min_value: dt.datetime,
     data: DataObject,
