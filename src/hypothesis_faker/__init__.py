187771e8bc0a6701f03985b01488af6938c50cee
Derek Wan <d.wan@icloud.com>
2021-05-08 14:30:50 +0800

    Update README

--- src/hypothesis_faker/__init__.py
+++ src/hypothesis_faker/__init__.py
@@ -1,45 +1,84 @@
-from hypothesis_faker.providers.color import color_names  # noqa: F401
-from hypothesis_faker.providers.color import colors  # noqa: F401
-from hypothesis_faker.providers.color import hex_colors  # noqa: F401
-from hypothesis_faker.providers.color import rgb_colors  # noqa: F401
-from hypothesis_faker.providers.color import rgb_css_colors  # noqa: F401
-from hypothesis_faker.providers.color import safe_hex_colors  # noqa: F401
-from hypothesis_faker.providers.file import file_extensions  # noqa: F401
-from hypothesis_faker.providers.file import file_names  # noqa: F401
-from hypothesis_faker.providers.file import file_paths  # noqa: F401
-from hypothesis_faker.providers.file import mime_types  # noqa: F401
-from hypothesis_faker.providers.file import unix_devices  # noqa: F401
-from hypothesis_faker.providers.file import unix_partitions  # noqa: F401
-from hypothesis_faker.providers.lorem import paragraphs  # noqa: F401
-from hypothesis_faker.providers.lorem import sentences  # noqa: F401
-from hypothesis_faker.providers.lorem import texts  # noqa: F401
-from hypothesis_faker.providers.lorem import words  # noqa: F401
-from hypothesis_faker.providers.person import first_names  # noqa: F401
-from hypothesis_faker.providers.person import first_names_female  # noqa: F401
-from hypothesis_faker.providers.person import first_names_male  # noqa: F401
+from hypothesis_faker.providers.color import color_names
+from hypothesis_faker.providers.color import colors
+from hypothesis_faker.providers.color import hex_colors
+from hypothesis_faker.providers.color import rgb_colors
+from hypothesis_faker.providers.color import rgb_css_colors
+from hypothesis_faker.providers.color import safe_hex_colors
+from hypothesis_faker.providers.file import file_extensions
+from hypothesis_faker.providers.file import file_names
+from hypothesis_faker.providers.file import file_paths
+from hypothesis_faker.providers.file import mime_types
+from hypothesis_faker.providers.file import unix_devices
+from hypothesis_faker.providers.file import unix_partitions
+from hypothesis_faker.providers.lorem import paragraphs
+from hypothesis_faker.providers.lorem import sentences
+from hypothesis_faker.providers.lorem import texts
+from hypothesis_faker.providers.lorem import words
+from hypothesis_faker.providers.person import first_names
+from hypothesis_faker.providers.person import first_names_female
+from hypothesis_faker.providers.person import first_names_male
 from hypothesis_faker.providers.person import first_names_nonbinary
-from hypothesis_faker.providers.person import language_names  # noqa: F401
-from hypothesis_faker.providers.person import last_names  # noqa: F401
-from hypothesis_faker.providers.person import last_names_female  # noqa: F401
-from hypothesis_faker.providers.person import last_names_male  # noqa: F401
-from hypothesis_faker.providers.person import last_names_nonbinary  # noqa: F401
-from hypothesis_faker.providers.person import names  # noqa: F401
-from hypothesis_faker.providers.person import names_female  # noqa: F401
-from hypothesis_faker.providers.person import names_male  # noqa: F401
-from hypothesis_faker.providers.person import names_nonbinary  # noqa: F401
-from hypothesis_faker.providers.person import prefixes  # noqa: F401
-from hypothesis_faker.providers.person import prefixes_female  # noqa: F401
-from hypothesis_faker.providers.person import prefixes_male  # noqa: F401
-from hypothesis_faker.providers.person import prefixes_nonbinary  # noqa: F401
-from hypothesis_faker.providers.person import suffixes  # noqa: F401
-from hypothesis_faker.providers.person import suffixes_female  # noqa: F401
-from hypothesis_faker.providers.person import suffixes_male  # noqa: F401
-from hypothesis_faker.providers.person import suffixes_nonbinary  # noqa: F401
-from hypothesis_faker.providers.phone_number import (  # noqa: F401
+from hypothesis_faker.providers.person import language_names
+from hypothesis_faker.providers.person import last_names
+from hypothesis_faker.providers.person import last_names_female
+from hypothesis_faker.providers.person import last_names_male
+from hypothesis_faker.providers.person import last_names_nonbinary
+from hypothesis_faker.providers.person import names
+from hypothesis_faker.providers.person import names_female
+from hypothesis_faker.providers.person import names_male
+from hypothesis_faker.providers.person import names_nonbinary
+from hypothesis_faker.providers.person import prefixes
+from hypothesis_faker.providers.person import prefixes_female
+from hypothesis_faker.providers.person import prefixes_male
+from hypothesis_faker.providers.person import prefixes_nonbinary
+from hypothesis_faker.providers.person import suffixes
+from hypothesis_faker.providers.person import suffixes_female
+from hypothesis_faker.providers.person import suffixes_male
+from hypothesis_faker.providers.person import suffixes_nonbinary
+from hypothesis_faker.providers.phone_number import country_calling_codes
+from hypothesis_faker.providers.phone_number import msisdns
+from hypothesis_faker.providers.phone_number import phone_numbers
+
+
+_ = {
+    color_names,
+    colors,
+    hex_colors,
+    rgb_colors,
+    rgb_css_colors,
+    safe_hex_colors,
+    file_extensions,
+    file_names,
+    file_paths,
+    mime_types,
+    unix_devices,
+    unix_partitions,
+    paragraphs,
+    sentences,
+    texts,
+    words,
+    first_names,
+    first_names_female,
+    first_names_male,
+    first_names_nonbinary,
+    language_names,
+    last_names,
+    last_names_female,
+    last_names_male,
+    last_names_nonbinary,
+    names,
+    names_female,
+    names_male,
+    names_nonbinary,
+    prefixes,
+    prefixes_female,
+    prefixes_male,
+    prefixes_nonbinary,
+    suffixes,
+    suffixes_female,
+    suffixes_male,
+    suffixes_nonbinary,
     country_calling_codes,
-)
-from hypothesis_faker.providers.phone_number import msisdns  # noqa: F401
-from hypothesis_faker.providers.phone_number import phone_numbers  # noqa: F401
-
-
-_ = first_names_nonbinary
+    msisdns,
+    phone_numbers,
+}
