e14c894e03e954e08029f96452a8b7ce1a856d9d
Derek Wan <d.wan@icloud.com>
2022-05-08 09:46:45 +0800

    Correct versions

--- src/hypothesis_faker/__init__.py
+++ src/hypothesis_faker/__init__.py
@@ -1,2 +1,5 @@
 from hypothesis_faker.strategies import addresses  # type: ignore # noqa: F401
 from hypothesis_faker.strategies import coordinates  # type: ignore # noqa: F401
+
+
+__version__ = "0.1.0"
